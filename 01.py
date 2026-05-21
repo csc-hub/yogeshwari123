e49f17d024467096c7ab275c83700025e0bf44d8
yogeshwari <yogeshwari123@gmail.com>
2026-05-21 15:51:15 +0530

    first project

---
+++ 01.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number: "))
+b=int(input("enter another number: "))  
+if a>b:
+    print("a is greater than b")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
