5eaf20a491fd585bc1acced42f230fda108745e3
yogeshwari <yogeshwari123@gmail.com>
2026-05-21 16:05:59 +0530

    second program

---
+++ 02.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number: "))
+b=int(input("enter another number: "))
+c=int(input("enter a third number: "))
+if a>b and a>c:
+    print("a is the greatest number")   
+if b>a and b>c:
+    print("b is the greatest number")
+if c>a and c>b:
+    print("c is the greatest number")
